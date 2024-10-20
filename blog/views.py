40950487991f76c0088ce804bc89bbe4fd16e466
JanainaSousa65 <janainacorreia65@hotmail.com>
2024-10-20 14:33:56 -0300

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,39 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Post  # ou o modelo que você está usando
-from .forms import PostForm 
+from django.shortcuts import render
 
+# Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
-    return render(request, 'blog/post_list.html', {'posts': posts})
-
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
-
-def post_new(request):
-     if request.method == "POST":
-         form = PostForm(request.POST)
-         if form.is_valid():
-             post = form.save(commit=False)
-             post.author = request.user
-             post.published_date = timezone.now()
-             post.save()
-             return redirect('post_detail', pk=post.pk)
-     else:
-         form = PostForm()
-     return render(request, 'blog/post_edit.html', {'form': form})
-
-def post_edit(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():  # Mover esta linha para dentro da condição POST
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm(instance=post)  # Inicializa o formulário com a instância do post
-
-    return render(request, 'blog/post_edit.html', {'form': form})
+    return render(request, 'blog/post_list.html', {})
